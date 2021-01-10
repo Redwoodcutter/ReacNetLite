34aa2ac32f2b6fafcc8a5966d90fc85ba3930ed0
redwoodcutter <oguzkumcular@gmail.com>
2021-01-10 15:10:33 +0300

    React Update

---
+++ client-app/src/App.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import './App.css';
+import axios from 'axios'
+
+
+class App extends Component{
+state = {
+  values: []
+}
+
+componentDidMount(){
+  axios.get('http://localhost:5000/api/values')
+    .then((response) => {
+      console.log(response);
+      this.setState({
+        values: response.data
+      })
+    })
+}
+
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+         <ul>
+           {this.state.values.map((value) => (
+             <li key={value.id}> {value.name}</li>
+           ))}
+         </ul>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
